86998bf5a8afd3b3cde5a57cd7bdaf49da6eef42
kaish <kaish@DEN-JPOOLLAT1>
2023-10-20 13:45:23 +0100

    Add project files.

---
+++ wwwroot/Models/FoodItems.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantWebApp.wwwroot.Models
+{
+    public class FoodItems
+    {
+        [Key]
+        public int ID { get;set; }
+        [StringLength(30)]
+        public string Item_name { get; set; }
+        [StringLength(225)]
+        public string Item_desc { get; set; }
+        public Nullable<bool> Available { get; set; }
+        public bool? Vegetarian { get; set; }
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "Money")]
+        public Nullable<decimal> Price { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+//the animation transition
+
+/*const allSections = document.querySelectorAll('.selection')
+
+const fadingSection = (entries, observer) => {
+    const [entry] = entries
+    console.log('hello')
+
+    if (!entry.isIntersecting) return
+    entry.target.classList.remove('section_hidden')
+}
+
+const sectionObserver = new IntersectionObserver(fadingSection, {
+    root: null,
+    threshold: 0.12
+})
+
+allSections.forEach(section => {
+    sectionObserver.observe(section)
+    section.classList.add('section_hidden')
+})*/
