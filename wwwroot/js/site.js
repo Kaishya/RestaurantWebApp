bb0fa3ae2c77e76bdd0e26ef97fc45cb899b4438
kaish <kaish@DEN-JPOOLLAT1>
2023-10-25 16:22:41 +0100

    Milestone 1

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,25 +3,3 @@
 
 // Write your JavaScript code.
 
-
-//the animation transition
-
-/*const allSections = document.querySelectorAll('.selection')
-
-const fadingSection = (entries, observer) => {
-    const [entry] = entries
-    console.log('hello')
-
-    if (!entry.isIntersecting) return
-    entry.target.classList.remove('section_hidden')
-}
-
-const sectionObserver = new IntersectionObserver(fadingSection, {
-    root: null,
-    threshold: 0.12
-})
-
-allSections.forEach(section => {
-    sectionObserver.observe(section)
-    section.classList.add('section_hidden')
-})*/
